34a8f0f34fe254842b7e8ff143ab303559dbe90b
OmerOK1 <96621576+OmerOK1@users.noreply.github.com>
2023-08-20 18:21:01 +0300

    fixed incorrect hirrarchy that caused problems rendering pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,17 @@
 //import React from 'react';
 //import logo from './logo.svg';
+import { Outlet } from 'react-router-dom';
 import './App.css';
 import Landing from './Components/LandingPage/Landing';
+import Routing from './Components/Routing/Routing';
 
 function App() {
   return (
     <div className="App">
-      <Landing/>
+      
+      <Routing/>
+      <Outlet/>
+      <div className='footer'>footer</div>
     </div>
   );
 }
