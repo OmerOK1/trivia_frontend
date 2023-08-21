b3e2b3d77352533a4ed9a2494af1809446a2b643
OmerOK1 <96621576+OmerOK1@users.noreply.github.com>
2023-08-21 19:58:09 +0300

    models and forms

---
+++ src/Components/Models/GameModel.tsx
@@ -0,0 +1,25 @@
+import { Category } from "./Enums/Category";
+import { Difficulty } from "./Enums/Difficulty";
+import { LayoutEnum } from "./Enums/LayoutEnum";
+
+export class GameModel{
+    public id?: number;
+    public title?: string;
+    public category?: Category;
+    public difficulty?: Difficulty;
+    public questionsPerRound?: number;
+    public layout?: LayoutEnum;
+    public url?: string;
+
+   
+
+    public constructor(id?: number, title?: string, category?: Category, difficulty?: Difficulty, questionsPerRound?: number, layout?: LayoutEnum, url?: string) {
+        this.id = id;
+        this.title = title;
+        this.category = category;
+        this.difficulty = difficulty;
+        this.questionsPerRound = questionsPerRound;
+        this.layout = layout;
+        this.url = url;
+    }
+}
