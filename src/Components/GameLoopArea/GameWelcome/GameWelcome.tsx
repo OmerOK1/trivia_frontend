0a58b499066ca0a0ba6fea704a7138b8cd527742
OmerOK1 <96621576+OmerOK1@users.noreply.github.com>
2023-09-05 15:34:09 +0300

    trivia game loop&made first components in it

---
+++ src/Components/GameLoopArea/GameWelcome/GameWelcome.tsx
@@ -0,0 +1,21 @@
+import { useNavigate } from "react-router-dom";
+import store from "../../../Redux/Store";
+import CustomButton from "../../Button/CustomButton";
+import CustomLink from "../../CustomLink/CustomLink";
+import QuestionPage from "../QuestionPage/QuestionPage";
+
+function GameWelcome(): JSX.Element {
+    const navigate = useNavigate();
+    const gameTitle = store.getState().gameReducer.game?.title;
+
+
+
+    return (
+        <div className="game-welcome-page">
+            <h1>Welcome to {gameTitle}!</h1>
+            <CustomLink to="/question">Play Now</CustomLink>
+        </div>
+    )
+}
+
+export default GameWelcome;
